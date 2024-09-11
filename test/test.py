55a47974e52d47d6471b35abe16018a6e3d9b2f3
Hridi Prova Debnath <111787740+Hridi1992@users.noreply.github.com>
2024-09-11 17:50:38 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -11,8 +11,10 @@ async def test_project(dut):
     dut._log.info("Start")
 
     # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
+    clock = Clock(dut.clk, 20, units="us")
+    clock_sys = Clock(dut.ui_in[2], 2, units="us")
     cocotb.start_soon(clock.start())
+    cocotb.start_soon(clock_sys.start())
 
     # Reset
     dut._log.info("Reset")
@@ -20,21 +22,25 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
+    dut.ui_in[3].value = 1
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    dut.ui_in[3].value = 0
 
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in[0].value = 1
+    dut.ui_in[1].value = 1
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # Wait for 100 clock cycles to see the output values
+    await ClockCycles(dut.clk, 100)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out[0].value == 0
+    assert dut.uo_out[1].value == 0
+    assert dut.uo_out[2].value == 1
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
